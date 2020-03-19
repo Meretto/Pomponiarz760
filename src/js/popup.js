6616919a505024945219094674e2ff46ea835a5b
Artur Merecik <https://github.com/Meretto>
2020-03-19 21:26:20 +0100

    Moved getting random message to separated function

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,11 +4,16 @@ document.addEventListener(
     const bg = chrome.extension.getBackgroundPage()
     document.querySelector("button").addEventListener("click", onclick, false);
 
-    function onclick() {  
-        const randomPasta = bg.pasty[Math.floor(Math.random() * bg.pasty.length)];
+    function onclick() {
+        const randomPasta = bg.pasty[getRandomInt(0, bg.pasty.length)];
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {message: randomPasta});  
+            chrome.tabs.sendMessage(tabs[0].id, {message: randomPasta});
         });
+    }
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
   },
   false
