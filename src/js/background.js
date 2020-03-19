09d0acc6c690cd879589020359d9bdee5bc5b35d
Artur Merecik <https://github.com/Meretto>
2020-03-19 21:07:59 +0100

    Fixed bug with not working plugin after install

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,13 @@
 window.pasty = new Array(0);
 
+chrome.runtime.onInstalled.addListener(function() {
+  const url = chrome.runtime.getURL("data/messages.json");
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => window.pasty = json.messages);
+});
+
 chrome.runtime.onStartup.addListener(function() {
   const url = chrome.runtime.getURL("data/messages.json");
 
