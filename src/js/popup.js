ea24a0fbf5de3bc46046915766c9018975953f42
Artur Merecik <https://github.com/Meretto>
2020-03-19 21:44:40 +0100

    Fixed out of range when getting random message

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,7 +5,7 @@ document.addEventListener(
     document.querySelector("button").addEventListener("click", onclick, false);
 
     function onclick() {
-        const randomPasta = bg.pasty[getRandomInt(0, bg.pasty.length)];
+        const randomPasta = bg.pasty[getRandomInt(0, bg.pasty.length - 1)];
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {message: randomPasta});
         });
